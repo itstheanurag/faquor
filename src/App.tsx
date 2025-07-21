18c6de305cbe62a447ac8bd1ae1ed3af6cd1013c
gaurav <gauravanurag36@gmail.com>
2025-07-21 23:27:18 +0530

    feat: add dark mode in the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,9 @@
-import { useState } from 'react';
-import ImageUploader from './component/ImageUploader';
-import MessengerScreen from './screens/MessangerScreen';
-import ScreenSelector from './component/ImageSelector';
-import DownloadButton from './component/DownloadButton';
-
 export default function App() {
-  const [screen, setScreen] = useState('messenger');
-  const [image, setImage] = useState<string | null>(null);
 
   return (
-    <div className="min-h-screen p-4 bg-gray-100">
-      <h1 className="text-2xl font-bold mb-4">Meme Conversation Generator</h1>
-      <ImageUploader onImageLoad={setImage} />
-      <ScreenSelector selected={screen} onSelect={setScreen} />
-      <div id="export-area" className="my-4 p-4 bg-white rounded shadow">
-        {screen === 'messenger' && <MessengerScreen image={image} />}
-        {/* Future: Add other screen components here */}
-      </div>
-      <DownloadButton />
+    <div className="min-h-screen flex items-center p-4">
+      You Suck 
+      {/* <ModeToggle /> */}
     </div>
   );
 }
---
+++ src/screens/InputOutputScreen.tsx
@@ -0,0 +1,10 @@
+const InputOutputScreen = () => {
+  return (
+    <div className="flex items-center justify-center">
+        <div className="">Input</div>
+        <div className="">Render Screen</div>
+    </div>
+  )
+}
+
+export default InputOutputScreen
